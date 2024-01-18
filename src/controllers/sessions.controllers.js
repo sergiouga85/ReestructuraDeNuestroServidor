c59bcbe4fed96443df6efa5644b67daefd90c7b3
Sergio Ugarte <sergiouga85@gmail.com>
2024-01-18 20:04:48 -0300

    commit modificacion sessions

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -1,12 +1,39 @@
+import { usersOnly } from '../middlewares/authorization.js';
+import { appendJwtAsCookie, removeJwtFromCookies } from '../controllers/authentication.business.js';
+import passport from 'passport';
+
+
 export const loginUser = async (req, res, next) => {
+    try {
+    passport.authenticate('localLogin', {
+        failWithError: true,
+        session: false
+      }),
+      appendJwtAsCookie,
     res['successfullPost'](req.user);
+    } catch (error) {
+        next(error);
+    }
 };
 
   
 export const getCurrentSessionUser = async (req, res, next) => {
+    try {
+    passport.authenticate('jwtAuth', {
+        session: false
+      }),
+      usersOnly,
     res['successfullGet'](req.user);
+    } catch (error) {
+        next(error);
+    }
 };
   
 export const logoutUser = async (req, res, next) => {
+    try {
+    removeJwtFromCookies,
     res['successfullDelete']();
+    } catch (error) {
+        next(error);
+    }
 };
--- src/routers/api/sessions.router.js
+++ src/routers/api/sessions.router.js
@@ -1,33 +1,10 @@
 import { Router } from 'express';
-import { appendJwtAsCookie, removeJwtFromCookies } from '../../controllers/authentication.business.js';
 import { loginUser, getCurrentSessionUser, logoutUser } from '../../controllers/sessions.controllers.js';
-import { usersOnly } from '../../middlewares/authorization.js';
-import passport from 'passport';
 
 
 export const sessionsRouter = Router();
 
-// login
-sessionsRouter.post('/',
-  passport.authenticate('localLogin', {
-    failWithError: true,
-    session: false
-  }),
-  appendJwtAsCookie,
-  loginUser
-);
 
-// view
-sessionsRouter.get('/current',
-  passport.authenticate('jwtAuth', {
-    session: false
-  }),
-  usersOnly,
-  getCurrentSessionUser
-);
-
-// logout
-sessionsRouter.delete('/current',
-  removeJwtFromCookies,
-  logoutUser
-);
+sessionsRouter.post('/', loginUser);
+sessionsRouter.get('/current', getCurrentSessionUser);
+sessionsRouter.delete('/current',logoutUser);
