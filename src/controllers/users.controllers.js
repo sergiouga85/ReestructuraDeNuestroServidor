43b46cebda002edc4694f0ca1d6fe00f27247277
Sergio Ugarte <sergiouga85@gmail.com>
2024-01-18 19:59:17 -0300

    tercer commit

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,23 +1,41 @@
 import {UserDAO} from '../dao/userDao.js'
+import { appendJwtAsCookie } from '../controllers/authentication.business.js';
+import { adminsOnly, usersOnly } from '../middlewares/authorization.js';
+import passport from 'passport';
 
 export const registerUser = async (req, res, next) => {
-    try {
-      res['successfullPost'](req.user);
-    } catch (error) {
+  try {
+    passport.authenticate('localRegister', {
+      failWithError: true,
+      session: false,
+    }),
+    appendJwtAsCookie,
+    res['successfullPost'](req.user);
+  } catch (error) {
       next(error);
-    }
-};
+  }
+}
   
 export const getCurrentUser = async (req, res, next) => {
   try {
+    passport.authenticate('jwtAuth', {
+      failWithError: true,
+      session: false,
+    }),
+    usersOnly,
     res['successfullGet'](req.user);
   } catch (error) {
     next(error);
   }
-};
+}
   
 export const getAllUsers = async (req, res, next) => {
     try {
+      passport.authenticate('jwtAuth', {
+        failWithError: true,
+        session: false,
+      }),
+      adminsOnly
       const usuarios = await UserDAO.findAllUsers()
       res['successfullGet'](usuarios);
     } catch (error) {
--- src/routers/api/users.router.js
+++ src/routers/api/users.router.js
@@ -1,39 +1,10 @@
 import { Router } from 'express';
-import { appendJwtAsCookie } from '../../controllers/authentication.business.js';
-import { adminsOnly, usersOnly } from '../../middlewares/authorization.js';
-import passport from 'passport';
 import { registerUser, getCurrentUser, getAllUsers} from '../../controllers/users.controllers.js';
 
 export const usersRouter = Router();
 
-usersRouter.post(
-  '/',
-  passport.authenticate('localRegister', {
-    failWithError: true,
-    session: false,
-  }),
-  appendJwtAsCookie,
-  registerUser
-);
-
-usersRouter.get(
-  '/current',
-  passport.authenticate('jwtAuth', {
-    failWithError: true,
-    session: false,
-  }),
-  usersOnly,
-  getCurrentUser
-);
-
-usersRouter.get(
-  '/',
-  passport.authenticate('jwtAuth', {
-    failWithError: true,
-    session: false,
-  }),
-  adminsOnly,
-  getAllUsers
-);
+usersRouter.post('/',registerUser);
+usersRouter.get('/current',getCurrentUser);
+usersRouter.get('/',getAllUsers); 
 
 
